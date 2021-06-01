849d8369cc3f21ef6fd4217b52222ab25ac92cd4
Davi Sieves <ralfsieves@gmail.com>
2021-06-01 17:42:38 +0200

    exercicio Davi

---
+++ Aula01.py
@@ -0,0 +1,7 @@
+num1 = int(input("informe o primeiro numero"))
+
+num2 = int(input("informe o segundo numero"))
+
+soma = num1 + num2
+multiplicacao = soma * num1
+print("o resultado da multiplicacao da multiplicao e {0}".format(multiplicacao))
---
+++ aula04.py
@@ -0,0 +1,5 @@
+metros = int(input("informe valor e metros: ")) 
+
+centimetros = (metros * 100)
+
+print("{0} metros em centimetros e {1}".format(metros, centimetros))
