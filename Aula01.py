8f8bfe91751633b9e5a95736ca086d457bff63ac
Ralf Sieves <ralfsievesgmail.com>
2021-06-01 17:59:55 +0200

    Alteração Ralf

--- Aula01.py
+++ Aula01.py
@@ -4,4 +4,6 @@ num2 = int(input("informe o segundo numero"))
 
 soma = num1 + num2
 multiplicacao = soma * num1
-print("o resultado da multiplicacao da multiplicao e {0}".format(multiplicacao))
+
+print(f"O Resultado da Multiplicação é  {multiplicacao}")
+
