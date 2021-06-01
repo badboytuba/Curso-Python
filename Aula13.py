61515ee4765392f97643f0463ec207cc79429737
Davi Sieves <ralfsieves@gmail.com>
2021-06-01 21:05:32 +0200

    andamentos das aulas

---
+++ Aula13.py
@@ -0,0 +1,17 @@
+num1 = int(input("informe o numero 1: "))
+num2 = int(input("informe o numero 2: "))
+num3 = int(input("informe o numero 3: "))
+num4 = int(input("informe o numero 4: "))
+
+q1 = num1 * num1
+q2 = num2 * num2
+q3 = num3 * num3
+q4 = num4 * num4
+
+if q3 >=1000:
+    print(q3)
+else:
+    print("num1: {0}, quadrado: {1}".format(num1,q1))
+    print("num1: {0}, quadrado: {1}".format(num1,q1))
+    print("num1: {0}, quadrado: {1}".format(num1,q1))
+    print("num1: {0}, quadrado: {1}".format(num1,q1))
